15acc702399593b74144921726926d1c162b1dc1
George Whelan <gwhelan@outlook.com>
2014-04-27 22:35:24 +0100

    Lap factory added

---
+++ RLCTelemetry/Utilities/Factories/LapFactory.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LapFactory.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RLCTelemetry.Utilities.Factories
+{
+    using System;
+    using RLCTelemetry.Stream.Data;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public static class LapFactory
+    {
+
+        //public static LapFactory()
+        //{
+               
+        //}
+
+        public static Lap NewLap()
+        {
+            Lap lap = new Lap();
+            return lap;
+        }
+    }
+}
