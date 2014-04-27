66c61667dbfbda9287724777db67f0aa7e5dd74b
George Whelan <gwhelan@outlook.com>
2014-04-27 22:35:33 +0100

    Milisecond converter added

---
+++ RLCTelemetry/Utilities/MilisecondConverter.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MilisecondConverter.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RLCTelemetry.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public static class MilisecondConverter
+    {
+        //public Time Convert(float miliseconds)
+        //{
+        //    // Takes in a float of miliseconds. Returns a new time formatted as 1:11.111.
+        //}
+    }
+}
