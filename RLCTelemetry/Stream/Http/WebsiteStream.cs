8de8242e79b9d2ebe1fd8f1f5c026b9910ec2bd8
George Whelan <gwhelan@outlook.com>
2014-04-28 10:24:08 +0100

    Update to .net 4.5

---
+++ RLCTelemetry/Stream/Http/WebsiteStream.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HttpClient.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RLCTelemetry.Stream.Http
+{
+    using System;
+    using System.Net.Http;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class WebsiteStream
+    {
+
+        public void WebsiteStream()
+        {
+            HttpClient stream = new HttpClient();
+
+            
+
+        }
+    }
+}
