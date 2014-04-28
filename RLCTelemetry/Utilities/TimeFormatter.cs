dd27c109083c715f60d86a2b9c066c88ce2e3067
George Whelan <gwhelan@outlook.com>
2014-04-28 21:46:05 +0100

    String.format rather than... the other way.

--- RLCTelemetry/Utilities/TimeFormatter.cs
+++ RLCTelemetry/Utilities/TimeFormatter.cs
@@ -6,21 +6,13 @@ namespace RLCTelemetry.Utilities
 
         public static string FormatFloat(float time)
         {
-            //return TimeSpan.FromMilliseconds((double)time).TotalMinutes;
             double dt = time;
-
-
-
             TimeSpan t = TimeSpan.FromSeconds(dt);
 
-            //string answer = string.Format("{1:D2}m:{2:D2}s:{3:D3}ms",
-            //    t.Minutes,
-            //    t.Seconds,
-            //    t.Milliseconds);
-
-            string answer = t.Minutes + ":" + t.Seconds + "." + t.Milliseconds;
-
-            //string answer = t.Minutes.ToString();
+            string answer = string.Format("{1:D2}:{2:D2}.{3:D3}",
+                t.Minutes,
+                t.Seconds,
+                t.Milliseconds);
 
             return answer;
         }
