17ce311a83acce2d7c999fb6fd1d73c8ec1476ba
George Whelan <gwhelan@outlook.com>
2014-05-12 17:09:09 +0100

    Small data stream changes

--- RLCTelemetry/Stream/UDP/UDPStream.cs
+++ RLCTelemetry/Stream/UDP/UDPStream.cs
@@ -14,6 +14,7 @@ namespace RLCTelemetry.Stream.UDP
     using RLCTelemetry.Stream.Data;
     using RLCTelemetry.Utilities.Factories;
     using RLCTelemetry.Utilities;
+using RLCTelemetry.Stream.Http;
 
     /// <summary>
     /// TODO: Update summary.
@@ -55,8 +56,12 @@ namespace RLCTelemetry.Stream.UDP
         private float sector2 = 0;
         private float sector3 = 0;
 
+        private WebsiteStream rlcwebsite;
+
         public UDPStream(IPAddress server, int port, MainWindow parent)
         {
+            this.rlcwebsite = new WebsiteStream();
+
             this.parent = parent;
             this.listenPort = port;
             this.ipAddress = server;
@@ -65,6 +70,8 @@ namespace RLCTelemetry.Stream.UDP
             this.sectors.Add(false);
             this.sectors.Add(false);
 
+
+            // Todo, make these
             keys.Add("time", 0);
             keys.Add("lap_time", 1);
             keys.Add("lap_distance", 2);
@@ -120,7 +127,6 @@ namespace RLCTelemetry.Stream.UDP
             keys.Add("brake_temperature_rear_right", 52);
             keys.Add("brake_temperature_front_left", 53);
             keys.Add("brake_temperature_front_right", 54);
-            // I'm guessing brake pressure or something.
             keys.Add("new_field18", 55);
             keys.Add("new_field19", 56);
             keys.Add("new_field20", 57);
@@ -133,13 +139,13 @@ namespace RLCTelemetry.Stream.UDP
             keys.Add("new_field_1302", 64);
             keys.Add("new_field_1303", 65);
 
-            Console.WriteLine("[Stream] Starting new stream");
+            //Console.WriteLine("[Stream] Starting new stream");
 
         }
 
         public void Stop()
         {
-            Console.WriteLine("[Stream] Stream turned off");
+            //Console.WriteLine("[Stream] Stream turned off");
             this.Running = false;
         }
 
@@ -148,7 +154,7 @@ namespace RLCTelemetry.Stream.UDP
             this.session = session;
             this.Running = true;
 
-            Console.WriteLine("[Stream] Stream is running");
+            //Console.WriteLine("[Stream] Stream is running");
 
             UdpClient listener = new UdpClient(this.listenPort);
             IPEndPoint groupEP = new IPEndPoint(this.ipAddress, this.listenPort);
@@ -184,6 +190,7 @@ namespace RLCTelemetry.Stream.UDP
 
                             // Top speed section.
                             this.session.UpdateTopSpeed(this.stream[7], this.lapcount + 1);
+                            this.currentlap.TopSpeed = this.stream[7];
 
 
                             // Sector 1 has changed.
@@ -193,51 +200,57 @@ namespace RLCTelemetry.Stream.UDP
                                 {
                                     this.sector1 = this.stream[49];
                                     this.sector1flag = true;
-                                    this.currentlap.Sector1 = this.sector1;
+                                    this.currentlap.UpdateSector(1, this.sector1);
                                     this.Sector(1, this.sector1);
                                 }
                                 
                             }
 
+                            // Sector 2 has changed.
                             if (this.stream[50] != 0)
                             {
                                 if (this.sector2flag == false)
                                 {
                                     this.sector2 = this.stream[50];
                                     this.sector2flag = true;
-                                    this.currentlap.Sector2 = this.sector2;
+                                    this.currentlap.UpdateSector(2, this.sector2);
                                     this.Sector(2, this.sector2);
                                 }
                             }
                         }
 
+
+                        // Crossed the finish line, new lap started.
                         if (this.stream[36] != this.lapcount)
                         {
-                            // Crossed the finish line, new lap started.
+                            
 
                             // The previous lap, minus the sector 1 + sector 2 time = sector 3.
                             this.sector3 = stream[62] - (this.sector1 + this.sector2);
-                            this.currentlap.Sector3 = this.sector3;
+                            this.currentlap.UpdateSector(3, this.sector3);
 
                             // Laptime should be the same as the three sectors.
+                            // Limit this to 3 dp.
                             this.currentlap.LapTime = this.stream[62];
-                            this.currentlap.LapTimeString = TimeFormatter.FormatFloat(this.stream[62]);
 
+                            // Safely delete this property.
+                            //this.currentlap.LapTimeString = TimeFormatter.FormatFloat(this.stream[62]);
+
+                            this.currentlap.FuelRemaining = this.stream[45];
+                            this.currentlap.RacePosition = (int)this.stream[39];
 
                             // Bit of console logging.
                             this.Sector(3, this.sector3);
-                            Console.WriteLine("[Stream] Previous lap: " + stream[62]);
+                            Console.WriteLine("[Stream] Previous lap: " + this.currentlap.LapTime);
 
-                            this.session.UpdateLastLapTime(this.currentlap.LapTimeString);
+                            this.session.UpdateLastLapTime(this.currentlap.LapTime);
 
+                            this.currentlap.UpdateLapNumber(this.lapcount + 1);
 
-                            this.currentlap.LapNumber = this.lapcount + 1;
-
-                            // Add final values for the lap here.
-                            //this.currentlap.CurrentFuel = stream[45];
-
-                            // Add the completed lap to the list of laps in the session.
+                            // Add the completed lap to the list of laps in the session and send it to the website.
                             this.session.AddLap(this.currentlap);
+                            // This can actually go inside the AddLap method.
+                            this.rlcwebsite.SubmitLap(this.currentlap, this.session.ID);
 
                             // Reset the currentlap to a brand new lap.
                             this.currentlap = new Lap();
