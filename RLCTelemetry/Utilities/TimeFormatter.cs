7fbad67adac6c8567d6ba8f20fe637c47f845627
George Whelan <gwhelan@outlook.com>
2014-05-12 17:09:28 +0100

    Time formatter decimal place bug fix

--- RLCTelemetry/Utilities/TimeFormatter.cs
+++ RLCTelemetry/Utilities/TimeFormatter.cs
@@ -9,7 +9,7 @@ namespace RLCTelemetry.Utilities
         {
             TimeFormatter.ts = TimeSpan.FromSeconds((double)time);
 
-            string answer = string.Format("{1:D2}:{2:D2}.{3:D3}",
+            string answer = string.Format("{0:D2}:{1:D2}.{2:D3}",
                 ts.Minutes,
                 ts.Seconds,
                 ts.Milliseconds);
